fe30fba230d4d3b5736215b6835097d66fb834c6
Bill Brinkert <william.brinkert@akqa.com>
2018-04-23 16:33:15 -0700

    initial commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => 
+  <div>
+    <p>Welcome to my second Gatsby page</p>
+    <Link to="/">back home</Link>
+  </div>
